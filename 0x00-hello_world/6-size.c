d41dea3fe9366bf5debe5849f5b618eb63502e52
koacha <koshishmael964@gmail.com>
2022-12-10 08:07:23 -0800

    a long commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Starts 
+ * Return: This returns 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %i byte($)\n", sizeof(char));
+	printf("Size of an int: %i byte(($)\n, sizeof(int));
+	printf("Size of a long int: %i byte($)\n, sizeof(ling int));
+	printf("Size of a long long int: %i byte($)\n", sizeof(long long int));
+	printf("Size of a float: %i byte($)\n", sizeof(float));
+	return(0);
+}
